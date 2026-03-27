9e23d00400a6c0bd4dd7f8048aa34f8b3b4dcafa
vitorcosta039 <vitorcosta588@gmail.com>
2026-03-27 18:47:47 -0300

    add reservations controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReservationsController } from './reservations/reservations.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ReservationsController],
   providers: [AppService],
 })
 export class AppModule {}
